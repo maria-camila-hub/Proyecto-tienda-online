eb7176db86f82cf96bb2400a021647afd9750326
maria-camila-hub <camilaloaizag@gmail.com>
2025-06-11 13:23:08 -0500

    Delete Proyecto_tienda directory

--- Proyecto_tienda/Backend/src/main/java/com/tienda/repository/ProductoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tienda.repository;
-
-import com.tienda.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {}
